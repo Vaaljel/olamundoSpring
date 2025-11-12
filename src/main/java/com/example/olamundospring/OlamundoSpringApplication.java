424b07e5f8560cb5f5fd24d0189dd7a1b5744acb
Vaaljel <vaaljoel108@gmail.com>
2025-11-12 06:43:05 +0000

    class saudação e recapitulação

--- src/main/java/com/example/olamundospring/OlamundoSpringApplication.java
+++ src/main/java/com/example/olamundospring/OlamundoSpringApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class OlamundoSpringApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(OlamundoSpringApplication.class, args);
+        SpringApplication.run(OlamundoSpringApplication.class, args);
 	}
 
 }
---
+++ src/main/java/com/example/olamundospring/dto/SaudacaoController.java
@@ -0,0 +1,14 @@
+package com.example.olamundospring.dto;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SaudacaoController {
+
+    @GetMapping("/saudacao/{nome}")
+    public String saudacao(@PathVariable String nome) {
+        return "Olá " + nome + "(via PathVariable)";
+    }
+}
